19e204a0cc0c93f2d7b7a412aa7e075f819d91ed
anna-flytis <a.mariakhina@gmail.com>
2023-06-13 09:11:40 -0600

    finished backend for generation

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const { Client } = require('pg');
 const cors = require('cors'); //security system 
 const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
 
 dotenv.config();
 const app = express();
 app.use(cors()); 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//AI configuration
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 // Database connection
 const client = new Client({
@@ -35,8 +46,33 @@ app.get('/api/images', (req, res) => {
     });
 });
 
+
+// SERVER PART
 app.get('/', (req, res) => {
   return res.status(200).send('Server is up');
+});
+
+
+//AI 
+app.post('/generate', async (req, res) => {
+  const { prompt, size } = req.body;
+  // validation
+  if(!prompt || !size){
+    return res.status(400).send('Bad request');
+  }
+  try {
+    const response = await openai.createImage({
+      prompt,
+      size,
+      n: 1,
+    });
+   const image_url =response.data[0].url;
+    return response.status(200).send({
+      src: image_url,
+    });
+  } catch (error) {
+      return response.status(500).send({error});
+  }
 });
 
 // Start the server
